37f8255bda0213a44fa68e5351b8bdf2aa36fc87
Your Name <you@example.com>
2018-11-03 15:20:07 -0400

    initialized red line stations with fixed latitude and longitude

---
+++ mbta.js
@@ -0,0 +1,51 @@
+var stops = [23];
+function initMap() {
+	sstat = {lat: 42.352271, lng: -71.05524200000001, name: "South Station"};
+	andrw = {lat: 42.330154, lng: -71.057655, name: "Andrew"};
+	portr = {lat: 42.3884, lng: -71.11914899999999, name: "Porter Square"};
+	harsq = {lat: 42.373362, lng: -71.118956, name: "Harvard Square"};
+	jfk = {lat: 42.320685, lng: -71.052391, name: "JFK/UMass"};
+	shmnl = {lat: 42.31129, lng: -71.053331, name: "Savin Hill"};
+	pktrm = {lat: 42.35639457, lng: -71.0624242, name: "Park Street"};
+	brdwy = {lat: 42.342622, lng: -71.056967, name: "Broadway"};
+	nqncy = {lat: 42.275275, lng: -71.029583, name: "North Quincy"};
+	smmnl = {lat: 42.29312583, lng: -71.06573796000001, name: "Shawmut"};
+	davis = {lat: 42.39674, lng: -71.121815, name: "Davis"};
+	alfcl = {lat: 42.395428, lng: -71.142483, name: "Alewife"};
+	knncl = {lat: 42.36249079, lng: -71.08617653, name: "Kendall/MIT"};
+	chmnl = {lat: 42.361166, lng: -71.070628, name: "Charles/MGH"};
+	dwnxg = {lat: 42.355518, lng: -71.060225, name: "Downtown Crossing"};
+	qnctr = {lat: 42.251809, lng: -71.005409, name: "Quincy Center"};
+	qamnl = {lat: 42.233391, lng: -71.007153, name: "Quincy Adams"};
+	asmnl = {lat: 42.284652, lng: -71.06448899999999, name: "Ashmont"};
+	wlsta = {lat: 42.2665139, lng: -71.0203369, name: "Wollaston"};
+	fldcr = {lat: 42.300093, lng: -71.061667, name: "Fields Corner"};
+	cntsq = {lat: 42.365486, lng: -71.103802, name: "Central Square"};
+	brntn = {lat: 42.2078543, lng: -71.0011385, name: "Braintree"};
+	
+	winds = [
+		{lat: 42.395428, lng: -71.142483},
+		{lat: 42.39674, lng: -71.121815},
+		{lat: 42.3884, lng: -71.11914899999999}, 
+		{lat: 42.373362, lng: -71.118956},
+		{lat: 42.365486, lng: -71.103802},
+		{lat: 42.36249079, lng: -71.08617653},
+		{lat: 42.361166, lng: -71.070628},
+		{lat: 42.35639457, lng: -71.0624242},
+		{lat: 42.355518, lng: -71.060225},
+		{lat: 42.352271, lng: -71.05524200000001},
+		{lat: 42.342622, lng: -71.056967},
+		{lat: 42.330154, lng: -71.057655},
+		{lat: 42.320685, lng: -71.052391},
+		{lat: 42.31129, lng: -71.053331},
+		{lat: 42.300093, lng: -71.061667},
+		{lat: 42.29312583, lng: -71.06573796000001},
+		{lat: 42.284652, lng: -71.06448899999999},
+		{lat: 42.31129, lng: -71.053331},
+		{lat: 42.275275, lng: -71.029583},
+		{lat: 42.251809, lng: -71.005409},
+		{lat: 42.233391, lng: -71.007153},
+		{lat: 42.2078543, lng: -71.0011385}
+	];
+
+}
